42d17ccacdef42c0078bf7083109cdccd28e4cfd
kevinr498 <94397439+kevinr498@users.noreply.github.com>
2022-10-11 14:40:27 -0400

    update

--- JS FOLDER/week 1/week2/teslaObjects/index.js
+++ JS FOLDER/week 1/week2/teslaObjects/index.js
@@ -1,35 +1,19 @@
-let dog = {
-    name: 'fido',
-    numberOfLegs: 4,
-    friends: [
-        {name: "sally", numberOfLegs: 4},
-        {name: "Teddy", numberOfLegs: 4}
-    ],
-    mom: {
-        name: "carrie",
-    },
-    talk: function() {
-        console.log('bark');
-    },
-
-};
-
-console.log(dog.friends[0].name);
-const { name, mom, friends } = dog;
-console.log(mom);
-console.log(name);
-console.log(friends)
-
-Object.freeze(dog);
-dog.test = "test";
-console.log(dog.test)
-
-
-class Animal2 { constructor (name, age, legs) {
-this.name = name,
-this.age = age,
-this.legs = legs
+class Animal2 {
+  constructor(
+    color,
+    trim,
+    wheels,
+    interior,
+    enhancedAutoPilot,
+    fullSelfDrivingCapability,
+    numberOfWheels,
+    numberOfDoors
+  ) {
+    color = String;
+    trim = String;
+    wheels = String;
+    interior = String;
+    enhancedAutoPilot = Boolean;
+    fullSelfDrivingCapability = Boolean;
+  }
 }
-talk() {console.log(
-    `my name is ${this.name}`
-)}}
