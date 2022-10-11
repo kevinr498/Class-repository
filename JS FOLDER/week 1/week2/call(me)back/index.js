599dfa91df7e4bf64f77e3b6798b61de9442e8b5
kevinr498 <94397439+kevinr498@users.noreply.github.com>
2022-10-11 11:20:55 -0400

    update

--- JS FOLDER/week 1/week2/call(me)back/index.js
+++ JS FOLDER/week 1/week2/call(me)back/index.js
@@ -1,35 +1,42 @@
-let dog = {
-    name: 'fido',
-    numberOfLegs: 4,
-    friends: [
-        {name: "sally", numberOfLegs: 4},
-        {name: "Teddy", numberOfLegs: 4}
-    ],
-    mom: {
-        name: "carrie",
+let callBack = {
+  user: { name: "Tommy", phonenumber: "434-566-8608" },
+  phoneCalls: [
+    {
+      nameOfCaller: "Fred",
+      numberOfCaller: "424-880-9857",
+      voiceMessage: `Greetings`,
     },
-    talk: function() {
-        console.log('bark');
+  ],
+  messages: [
+    {
+      messenger: "Fred",
+      email: "fred@email.com",
+      message: "Greetings",
     },
-
+  ],
 };
 
-console.log(dog.friends[0].name);
-const { name, mom, friends } = dog;
-console.log(mom);
-console.log(name);
-console.log(friends)
+const { user, phoneCalls, messages } = callBack;
 
-Object.freeze(dog);
-dog.test = "test";
-console.log(dog.test)
-
-
-class Animal2 { constructor (name, age, legs) {
-this.name = name,
-this.age = age,
-this.legs = legs
+function missedPhoneCall() {
+  callBack.phoneCalls.push({
+    nameOfCaller: "Fred",
+    numberOfCaller: "424-880-9857",
+    voiceMessage: `Greetings ${callBack.user.name} please call me back`,
+  });
+  readOutMessages();
 }
-talk() {console.log(
-    `my name is ${this.name}`
-)}}
+
+function readOutMessages() {
+  console.log(callBack.phoneCalls);
+  console.log(callBack.messages);
+}
+
+function missedMessages() {
+  callBack.phoneCalls.push({
+    messenger: "Fred",
+    email: "fred@email.com",
+    message: `Greetings ${callBack.user.name} please email me back`,
+  });
+  readOutMessages();
+}
