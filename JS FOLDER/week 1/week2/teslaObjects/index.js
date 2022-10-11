0c44970c65466bffe1178c28d1f9bf0924c4f61a
kevinr498 <94397439+kevinr498@users.noreply.github.com>
2022-10-11 16:06:28 -0400

    update

--- JS FOLDER/week 1/week2/teslaObjects/index.js
+++ JS FOLDER/week 1/week2/teslaObjects/index.js
@@ -1,13 +1,11 @@
-class Animal2 {
+class Vehicle {
   constructor(
     color,
     trim,
     wheels,
     interior,
     enhancedAutoPilot,
-    fullSelfDrivingCapability,
-    numberOfWheels,
-    numberOfDoors
+    fullSelfDrivingCapability
   ) {
     color = String;
     trim = String;
@@ -16,4 +14,56 @@ class Animal2 {
     enhancedAutoPilot = Boolean;
     fullSelfDrivingCapability = Boolean;
   }
+
+  #numberOfWheels(wheels) {
+    wheels = 4;
+  }
+  #numberOfDoors(doors) {
+    doors = 4;
+  }
+}
+function details2() {
+  return `Color is ${this.color}, trim is ${this.trim}, wheels are ${
+    this.wheels
+  }, interior is ${this.interior}, ehanced auto pilot is ${
+    this.enhancedAutoPilot ? "selected" : "not selected"
+  }, and full self driving mode is ${
+    this.fullSelfDrivingCapability ? "selected" : "not selected"
+  }`;
+}
+
+function details() {
+  return `Your Model S has a ${this.color} exterior color with the ${
+    this.wheels
+  } wheel package and ${
+    this.interior
+  } interior. This vehicle has Enhanced Auto Pilot ${
+    this.enhancedAutoPilot ? "enabled" : "disabled"
+  } and Full Self Driving Capability ${
+    this.fullSelfDrivingCapability ? "enabled" : "disabled"
+  }.`;
+}
+
+function getValues() {
+  this.color = document.getElementById("colorOptions").value;
+  this.wheels = document.getElementById("wheelOptions").value;
+  this.trim = document.getElementById("trimOptions").value;
+  this.interior = document.getElementById("interiorOptions").value;
+  this.enhancedAutoPilot = document.getElementById("enhancedfeatures").value;
+  this.fullSelfDrivingCapability = document.getElementById("fSDCF").value;
+  plaid();
+  text();
+  console.log(details());
+  return false;
+}
+
+function plaid() {
+  if (this.trim === "Plaid") return console.log("the top speed is 200");
+  else console.log("the top speed is 149");
+}
+
+function text() {
+  const textElement = document.createElement("p");
+  textElement.innerText = details();
+  document.body.appendChild(textElement);
 }
