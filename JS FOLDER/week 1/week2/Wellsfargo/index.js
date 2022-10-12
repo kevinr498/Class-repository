64e1b62f918b6fec2fbc3213317798005c78297a
kevinr498 <94397439+kevinr498@users.noreply.github.com>
2022-10-12 12:57:50 -0400

    Update

--- JS FOLDER/week 1/week2/Wellsfargo/index.js
+++ JS FOLDER/week 1/week2/Wellsfargo/index.js
@@ -1,37 +1,59 @@
-class WellsFargoEverydayCheckingPlus {
-  constructor(
-    color,
-    trim,
-    wheels,
-    interior,
-    enhancedAutoPilot,
-    fullSelfDrivingCapability,
-    vars
-  ) {
-    color = String;
-    trim = String;
-    wheels = String;
-    interior = String;
-    enhancedAutoPilot = Boolean;
-    fullSelfDrivingCapability = Boolean;
-    vars = vars;
+class WellsFargoEverydayChecking {
+  constructor(name, balance) {
+    this.name = name;
+    this.balance = balance;
+  }
+  addMoney(value) {
+    this.balance += value;
+  }
+
+  minusMoney(value) {
+    this.balance -= value;
+  }
+
+  showBalance() {
+    console.log(this.balance);
+    return this.balance;
   }
 }
 
-function WellsFargoEverydayChecking(name, balance) {
-  this.name = name;
-  this.balance = balance;
+class WellsFargoEverydayCheckingPlus extends WellsFargoEverydayChecking {
+  constructor(name, balance) {
+    super(name, balance);
+  }
+  minusMoney(value) {
+    this.balance -= value;
+    if (this.balance <= 0) {
+      return (
+        (this.balance -= 5) &&
+        console.log(
+          `An overdraft fee has been applied to your account. Your new balance is ${customerPlus.showBalance()}`
+        )
+      );
+    }
+  }
 }
 
-WellsFargoEverydayChecking.prototype.addMoney = function (value) {
-  this.balance += value;
-};
+let customer = new WellsFargoEverydayChecking("Kevin", 200000);
+let customerPlus = new WellsFargoEverydayCheckingPlus("Andrew", 0);
+customerPlus.minusMoney(20);
+customerPlus.addMoney(100);
+customerPlus.showBalance();
 
-WellsFargoEverydayChecking.prototype.minusMoney = function (value) {
-  this.balance -= value;
-};
+// function WellsFargoEverydayChecking(name, balance) {
+//   this.name = name;
+//   this.balance = balance;
+// }
 
-WellsFargoEverydayChecking.prototype.showBalance = function () {
-  console.log(this.balance);
-  return this.balance;
-};
+// WellsFargoEverydayChecking.prototype.addMoney = function (value) {
+//   this.balance += value;
+// };
+
+// WellsFargoEverydayChecking.prototype.minusMoney = function (value) {
+//   this.balance -= value;
+// };
+
+// WellsFargoEverydayChecking.prototype.showBalance = function () {
+//   console.log(this.balance);
+//   return this.balance;
+// };
