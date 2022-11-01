71435adda85b7e924d3fb2baf5797054091f1b72
kevinr498 <94397439+kevinr498@users.noreply.github.com>
2022-11-01 12:20:53 -0400

    update

---
+++ NodeJS/week1/commandlineTool/file.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+
+module.exports = {
+  save: (jsObject) => {
+    return new Promise((resolve, reject) => {
+      fs.writeFile("data.json", JSON.stringify(jsObject), (err) => {
+        if (err) reject(err);
+
+        resolve("JSON saved to a file");
+      });
+    });
+  },
+
+  read: () => {
+    return new Promise((resolve, reject) => {
+      fs.readFile("data.json", "utf-8", (err, data) => {
+        if (err) reject(err);
+        // console.log(data);
+        resolve(JSON.parse(data));
+      });
+    });
+  },
+};
--- NodeJS/week1/commandlineTool/index.js
+++ NodeJS/week1/commandlineTool/index.js
@@ -1,7 +1,27 @@
 const axios = require("axios");
+const fs = require("fs");
+const { save, read } = require("./file");
 
+const name = "Ross";
 axios
-  .get("http://universities.hipolabs.com/search?country=United+States")
+  .get("https://api.agify.io/?name=meelad", {
+    params: {
+      name: name,
+    },
+  })
   .then(async (res) => {
-    console.log(res.data);
+    console.log("Age:", res.data.age, res.data);
+    const age = res.data.age;
+    const count = res.data.count;
+    await save({ value: [{ age, name, count }] });
+    console.log(await read());
+  });
+axios
+  .get("https://api.genderize.io/?name=luc", {
+    params: {
+      name: name,
+    },
+  })
+  .then(async (res) => {
+    // console.log("Probability:", res.data.probability, res.data.gender);
   });
