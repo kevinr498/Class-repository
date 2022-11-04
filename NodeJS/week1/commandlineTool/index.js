670e2b5a111a16b18cb8b6a28497d79dc5dcd6ef
kevinr498 <94397439+kevinr498@users.noreply.github.com>
2022-11-04 16:37:33 -0400

    update

--- NodeJS/week1/commandlineTool/index.js
+++ NodeJS/week1/commandlineTool/index.js
@@ -1,41 +1,39 @@
 class Name {
-  constructor(name, vars) {
+  constructor(name) {
     this.name = name;
-    this.vars = vars;
-  }
-  setName() {
-    this.name = document.getElementById("nameId").value;
-    console.log(this.name);
   }
 }
 
-vars((this.name = String));
-
 const axios = require("axios");
-const fs = require("fs");
-const { stringify } = require("querystring");
 const { save, read } = require("./file");
+new Name((this.name = String));
 
-const name = "Ross";
+function setName() {
+  this.name = document.getElementById("nameId").value;
+  console.log(this.name);
+  return false;
+}
+
 axios
   .get("https://api.agify.io/?name=meelad", {
     params: {
-      name: name,
+      name: this.name,
     },
   })
   .then(async (res) => {
     console.log("Age:", res.data.age, res.data);
     const age = res.data.age;
     const count = res.data.count;
+    const name = this.name;
     await save({ value: [{ age, name, count }] });
     console.log(await read());
   });
 axios
   .get("https://api.genderize.io/?name=luc", {
     params: {
-      name: name,
+      name: this.name,
     },
   })
   .then(async (res) => {
-    // console.log("Probability:", res.data.probability, res.data.gender);
+    console.log("Probability:", res.data.probability, res.data.gender);
   });
