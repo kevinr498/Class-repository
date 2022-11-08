ae683f1179671344ab2cc1c467c0c221796bc2a0
kevinr498 <94397439+kevinr498@users.noreply.github.com>
2022-11-08 11:41:43 -0500

    update

---
+++ NodeJS/week1/week2/class/create_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    haircolor varchar(255)
+);
+CREATE TABLE comments (
+    id SERIAL PRIMARY KEY,
+    comment_text VARCHAR,
+    user_id INTEGER REFERENCE users(id)
+);
