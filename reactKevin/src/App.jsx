9d3f557a6ba743761c8e05154fcd323224c73fef
kevinr498 <94397439+kevinr498@users.noreply.github.com>
2023-01-20 21:45:43 -0500

    update

--- reactKevin/src/App.jsx
+++ reactKevin/src/App.jsx
@@ -1,20 +1,35 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { createElement } from "react";
+import { createRoot } from "react-dom/client";
 import "./App.css";
 import axios from "axios";
 
 function App() {
   const [text, setTextValue] = useState("");
+  const [count, setCount] = useState(0);
 
   const handleSubmit = (event) => {
     console.log("Created Image Link");
+    const createImage = createElement(
+      "img",
+      { src: imageSrc, id: "imageId" },
+      null
+    );
+    if (count % 2 == 0) {
+      const rootNode = document.getElementById("image");
+      const rootRemove = document.getElementById("imageId");
+      rootNode.removeChild(rootRemove);
+    } else {
+      const root = createRoot(document.getElementById("image"));
+      root.render(createImage);
+    }
     console.log(imageSrc);
     event.preventDefault();
-    document.getElementById("image").appendChild(createImage);
   };
+  const buttonValue = count % 2 == 0 ? "Go" : "Reset";
+
   const imageSrc = "https://source.unsplash.com/random?" + text;
-  const createImage = createElement("img", { src: imageSrc }, null);
 
   return (
     <div className="App">
@@ -23,21 +38,23 @@ function App() {
       </div>
       <h4>Search (separate terms with commas - no spaces)</h4>
       <div className="card">
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className="formInput">
           <input
             type="text"
             onChange={(event) => setTextValue(event.target.value)}
             value={text}
+            placeholder="Search for random photo"
           ></input>
-          <button className="imageSubmitButton" type="submit">
-            GO
+          <button
+            className="imageSubmitButton"
+            onClick={() => setCount((count) => count + 1)}
+            type="submit"
+          >
+            {buttonValue}
           </button>
         </form>
         <div id="image"></div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   );
 }
